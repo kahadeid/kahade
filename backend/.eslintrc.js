cf8d05408114ef2737246e5946b2d99940666c73
kahade <94228657+kahadeid@users.noreply.github.com>
2026-02-16 02:51:15 +0700

    Relax TypeScript ESLint rules
    Modified ESLint rules to allow more flexibility in TypeScript usage.

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -17,11 +17,18 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    // More relaxed rules for builds
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-console': 'off',
+    'prefer-const': 'warn',
+    'no-var': 'warn',
   },
 };
